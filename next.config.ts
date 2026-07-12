57437c5bd7c41ac4111639ec6973c9dc291c39cf
panjivr <vatorrohmanpanji@gmail.com>
2026-07-12 23:01:25 +0700

    Deploy static site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
